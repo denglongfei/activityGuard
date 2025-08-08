8dc0889551515e2bbd197079f44392d10b2e4b74
denglongfei <41456814+denglongfei@users.noreply.github.com>
2025-08-08 12:05:17 +0800

    feat:jdk 11

--- plugin_main/build.gradle.kts
+++ plugin_main/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
 
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 apply(from = "../maven_local.gradle")
